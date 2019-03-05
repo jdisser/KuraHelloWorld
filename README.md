bdac960b3350dac5f7262524c16d3c702000e737
jdisser <jimdisser@altocirrusapps.com>
2019-03-04 19:43:54 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World OSGI
+A component for KURA running in Raspberry Pi
---
+++ src/com/altocirrusapps/hello_osgi/HelloOsgi.java
@@ -0,0 +1,26 @@
+package com.altocirrusapps.hello_osgi;
+
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloOsgi {
+
+    private static final Logger s_logger = LoggerFactory.getLogger(HelloOsgi.class);
+
+    private static final String APP_ID = "com.altocirrusapps.hello_osgi";
+
+    protected void activate(ComponentContext componentContext) {
+
+        s_logger.info("Bundle " + APP_ID + " has started");
+
+        s_logger.debug(APP_ID + ": this is a debug message");
+    }
+
+    protected void deactivate(ComponentContext componentContext) {
+
+        s_logger.info("Bundle " + APP_ID + " has stopped");
+
+    }
+
+}
